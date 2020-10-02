b6cb5ab851de6d16540524c4525842e9d13b9555
Ishan Jain <41587216+jainishan28@users.noreply.github.com>
2020-10-02 17:25:39 +0530

    Add files via upload

---
+++ includes/classess/Constants.php
@@ -0,0 +1,20 @@
+<?php
+
+    class Constants{
+        public static $passwordDoNoMatch = "Your Passwords Don't Match";
+        public static $passwordNotAlphanumeric = "Your Passwords can only contain numbers and letters";
+        public static $passwordCharacters = "Your Password must be between 5 and 30 Characters";
+        public static $emailInvalid = "Your Email is invalid";
+        public static $emailsDoNotMatch = "Your Emails Don't Match";
+        public static $emailTaken = "This Email already exists";
+        public static $lastNameCharacters = "Your Last Name must be between 2 and 25 Characters";
+        public static $firstNameCharacters = "Your First Name must be between 2 and 25 Characters";
+        public static $usernameCharacters = "Your Username must be between 5 and 25 Characters";
+        public static $usernameTaken = "This Username already exists";
+
+
+        public static $loginFailed =  "Your Username or Password is Incorrect ";
+    }
+
+
+?>
